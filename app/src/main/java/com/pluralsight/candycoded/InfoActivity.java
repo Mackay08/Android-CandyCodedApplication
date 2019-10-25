8faac7da2293c2efed867c56b627e8bc9d3ab61b
Mark-Anthony Sappor <mackay.sappor@gmail.com>
2019-10-25 11:11:59 +0000

    Created the phoneIntent method Made the phone number textView clickable and connected it to the phoneIntent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -45,7 +45,5 @@ public class InfoActivity extends AppCompatActivity {
         Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
         phoneIntent.setData(uriNumber);
         startActivity(phoneIntent);
-
-
     }
 }
